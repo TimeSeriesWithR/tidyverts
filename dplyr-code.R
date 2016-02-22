864a8a47aa507e85b36dc11e432b66406e027bf1
DragonflyStats <kobriendublin@gmail.com>
2016-02-22 21:39:14 +0000

    Update dplyr-code.R

--- dplyr-code.R
+++ dplyr-code.R
@@ -1,26 +1,24 @@
-This exercise can be run entirely on `try.jupyter.org`
+# This exercise can be run entirely on `try.jupyter.org`
 
 ####Part A. Data preparation 
-<!------------------------------------------------------------------>
-1. First we load the two packages that we are going to use: *dplyr* and *nycflights13*. There will be a warning message that we can ignore.
+# 1. First we load the two packages that we are going to use: *dplyr* and *nycflights13*. There will be a warning message that we can ignore.
 
 library(dplyr)
 library(nycflights13)
 
-2. We will use the ***flights*** data set on *nycflights13*. To find out the number of rows (cases) and columns (variables) we can use the `dim()` command. The `summary()` command will give us a summary of the data.
+# 2. We will use the ***flights*** data set on *nycflights13*. To find out the number of rows (cases) and columns (variables) we can use the `dim()` command. The `summary()` command will give us a summary of the data.
 
 dim(flights)
 summary(flights)
 glimpse(flights)
 
-3. For ease of use, lets just work with 5 cases. We can use the `select_n()` command to select 5 cases from the data set.
+# 3. For ease of use, lets just work with 5 cases. We can use the `select_n()` command to select 5 cases from the data set.
 
 set.seed(123)
 data <- sample_n(flights, 5)
 
 #### Basic method of use select() 
-<!------------------------------------------------------------------>
-4. Here we can select a set of variables by name
+# 4. Here we can select a set of variables by name
 
 select(data, year)
 select(data, year, month, day)
@@ -29,7 +27,7 @@ select(data, year:day)
 select(data, -year, -month, -day)
 select(data, -(year:day))
 
-5. We can also select a set of variables by index.
+# 5. We can also select a set of variables by index.
 
 select(data, 1, 2, 3)
 select(data, 1:3)
@@ -37,14 +35,14 @@ select(data, 1:3)
 select(data, -1, -2, -3)
 select(data, -(1:3))
 
-6. Extending the last two examples to more advanced examples.
+# 6. Extending the last two examples to more advanced examples.
 
 select(data, year:day, -month)
 select(data, -(year:day), month)
 select(data, 1:3, -2)
 select(data, -(1:3), 2)
 
-7. Selecting variables based on characteristics of the variable name
+# 7. Selecting variables based on characteristics of the variable name
 
 # Utility functions of select()
 select(data, starts_with("arr"))
